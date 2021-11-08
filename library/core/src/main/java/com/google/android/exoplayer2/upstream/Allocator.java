1516e41cd9b6c5c0f4cb45094561e37d52198529
Ronxu1 <rongxingxu@126.com>
2021-11-08 22:26:21 +0800

    --content=格式化

--- library/core/src/main/java/com/google/android/exoplayer2/upstream/Allocator.java
+++ library/core/src/main/java/com/google/android/exoplayer2/upstream/Allocator.java
@@ -15,42 +15,48 @@
  */
 package com.google.android.exoplayer2.upstream;
 
-/** A source of allocations. */
+/**
+ * A source of allocations.
+ */
 public interface Allocator {
-
-  /**
-   * Obtain an {@link Allocation}.
-   *
-   * <p>When the caller has finished with the {@link Allocation}, it should be returned by calling
-   * {@link #release(Allocation)}.
-   *
-   * @return The {@link Allocation}.
-   */
-  Allocation allocate();
-
-  /**
-   * Releases an {@link Allocation} back to the allocator.
-   *
-   * @param allocation The {@link Allocation} being released.
-   */
-  void release(Allocation allocation);
-
-  /**
-   * Releases an array of {@link Allocation}s back to the allocator.
-   *
-   * @param allocations The array of {@link Allocation}s being released.
-   */
-  void release(Allocation[] allocations);
-
-  /**
-   * Hints to the allocator that it should make a best effort to release any excess {@link
-   * Allocation}s.
-   */
-  void trim();
-
-  /** Returns the total number of bytes currently allocated. */
-  int getTotalBytesAllocated();
-
-  /** Returns the length of each individual {@link Allocation}. */
-  int getIndividualAllocationLength();
+    
+    /**
+     * Obtain an {@link Allocation}.
+     *
+     * <p>When the caller has finished with the {@link Allocation}, it should be returned by calling
+     * {@link #release(Allocation)}.
+     *
+     * @return The {@link Allocation}.
+     */
+    Allocation allocate();
+    
+    /**
+     * Releases an {@link Allocation} back to the allocator.
+     *
+     * @param allocation The {@link Allocation} being released.
+     */
+    void release(Allocation allocation);
+    
+    /**
+     * Releases an array of {@link Allocation}s back to the allocator.
+     *
+     * @param allocations The array of {@link Allocation}s being released.
+     */
+    void release(Allocation[] allocations);
+    
+    /**
+     * Hints to the allocator that it should make a best effort to release any excess {@link
+     * Allocation}s.
+     */
+    void trim();
+    
+    /**
+     * Returns the total number of bytes currently allocated.
+     */
+    int getTotalBytesAllocated();
+    
+    /**
+     * Returns the length of each individual {@link Allocation}.
+     */
+    int getIndividualAllocationLength();
 }
